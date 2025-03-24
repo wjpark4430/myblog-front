cb468de5dab0fe720d4c604f4e006815d27c641b
woojin <pwj4na2@gmail.com>
2025-03-24 13:48:33 +0900

    feat: initial commit

---
+++ front/src/main/java/com/blog/front/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.blog.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+   
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+}
